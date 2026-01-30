bf4ac249d89fcd0e0f056d2f3bf1aabc89f3bfba
babiguling12 <sudarmaputra277@gmail.com>
2026-01-30 20:35:07 +0800

    feat: menambahkan migration

---
+++ database/migrations/2026_01_29_140539_create_teachers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('name');
+            $table->string('nip');
+            $table->string('position');
+            $table->string('major');
+            $table->string('subject')->nullable();
+            $table->string('photo')->nullable();
+
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('education');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
---
+++ database/migrations/2026_01_29_142125_create_calender_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calender_events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date')->nullable();
+
+            $table->enum('category', [
+                'akademik',
+                'kegiatan',
+                'libur',
+                'ujian'
+            ]);
+
+            $table->string('color')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calender_events');
+    }
+};
