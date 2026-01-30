862db6d803a936317bc206ac7f15592b6bc9ebb3
babiguling12 <sudarmaputra277@gmail.com>
2026-01-30 20:36:27 +0800

    feat: menambahkan halaman kategori berita

---
+++ resources/views/admin/kategori-berita.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot:metaTitle>Halaman Kategori Berita</x-slot:metaTitle>
+    <x-slot:metaDesc>Ini halaman cuma buat kategori berita aja</x-slot:metaDesc>
+    <x-slot:title>Kategori Berita</x-slot:title>
+
+    <h1 class="font-display text-3xl font-bold">Kategori Berita</h1>
+</x-app-layout>
