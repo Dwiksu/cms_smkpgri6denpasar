9570cea080329bf429ec6308ae27ee240edaeac1
babiguling12 <sudarmaputra277@gmail.com>
2026-02-03 17:42:18 +0800

    feat (admin) : menambahkan data pada seeder

--- database/seeders/StatSeeder.php
+++ database/seeders/StatSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Stat;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,33 @@ class StatSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Stat::factory()->create(
+            [
+                'key' => 'siswa',
+                'label' => 'Siswa Aktif',
+                'value' => 0
+            ]
+        );
+        Stat::factory()->create(
+            [
+                'key' => 'guru',
+                'label' => 'Tenaga Pendidik',
+                'value' => 0
+            ]
+        );
+        Stat::factory()->create(
+            [
+                'key' => 'jurusan',
+                'label' => 'Program Keahlian',
+                'value' => 0
+            ]
+        );
+        Stat::factory()->create(
+            [
+                'key' => 'mitra',
+                'label' => 'Mitra Industri',
+                'value' => 0
+            ]
+        );
     }
 }
