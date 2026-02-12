147e1dc17f20f0139904c8f6ae415ca229ecdc8c
Dwiksu <dwiik.kusuma@gmail.com>
2026-02-13 00:30:25 +0800

    feat: update HomeController to include About, Major, News, and GalleryAlbum data;  update home page appearance; add new assets for background and overlays

--- app/Http/Controllers/public/HomeController.php
+++ app/Http/Controllers/public/HomeController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\public;
 
 use App\Http\Controllers\Controller;
 use App\Models\Hero;
+use App\Models\About;
+use App\Models\GalleryAlbum;
+use App\Models\Major;
+use App\Models\News;
+use App\Models\Stat;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,6 +16,12 @@ class HomeController extends Controller
     public function index()
     {
         $hero = Hero::firstOrFail();
-        return view('public.home.index', compact('hero'));
-    }    
+        $about = About::firstOrFail();
+        $stats = Stat::all();
+        $majors = Major::getMajor();
+        $news = News::latest()->paginate(6);
+
+        $galleries = galleryAlbum::latest()->paginate(5);
+        return view('public.home.index', compact('hero', 'about', 'stats', 'majors', 'news', 'galleries'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 /* HALAMAN PUBLIK */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('public.home.index');
 
 /* HALAMAN ADMIN */
 
