bf45cd8a45e62a235f7be3e0832353e5c73645d2
babiguling12 <sudarmaputra277@gmail.com>
2026-02-17 00:07:00 +0800

    feat : update isi seeder

--- database/seeders/SchoolSeeder.php
+++ database/seeders/SchoolSeeder.php
@@ -17,9 +17,14 @@ class SchoolSeeder extends Seeder
             'short_name' => 'SMK PGRI 6 DENPASAR',
             'full_name' => 'Sekolah Menengah Kejuruan PGRI 6 DENPASAR',
             'address' => 'Jl. Tukad Gerinding No. 21 A Panjer, Kec. Denpasar Selatan, Kota Denpasar, Bali.',
-            'phone' => '-',
-            'email' => '-',
-            'website' => 'https://smkpgri6denpasar.sch.id'
+            'email' => 'smkpgri.enam@yahoo.co.id',
+            'office_phone' => '08113860660',
+            'website' => 'https://smkpgri6denpasar.sch.id',
+            'facebook' => 'https://www.facebook.com/sadgriska.jaya.3?mibextid=ZbWKwL',
+            'instagram' => 'https://www.instagram.com/sadgriska',
+            'youtube' => 'https://www.youtube.com/@smkpgri6denpasar911',
+            'tiktok' => 'https://www.tiktok.com/@sadgriska_dps',
+            'whatsapp_phone' => '+62 821-4639-0517',
         ]);
     }
 }
