5a9236dc265a5b40f8ff755ab37344b8e4d97fcf
babiguling12 <sudarmaputra277@gmail.com>
2026-02-22 14:27:34 +0800

    feat : menambahkan middleware baru

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'desktop.only' => \App\Http\Middleware\DesktopOnly::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
